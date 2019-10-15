2f715e83410bf775ebedff324aa4af3c129525b6
RodriguezOswaldo <owner2303@gmail.com>
2019-10-15 08:59:15 -0400

    using (cast)

--- Program.cs
+++ Program.cs
@@ -6,9 +6,15 @@ namespace TypeConversion
     {
         static void Main(string[] args)
         {
+            /*
             byte b = 1;
             int i = b;
             Console.WriteLine(i);
+            */
+            int i = 1;
+            //Specifying the cast. (variable type)
+            byte b = (byte) i;
+            Console.WriteLine(b);
         }
     }
 }
